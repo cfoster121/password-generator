12b77d9b298467ca5f5f99407455e82ac1bad28b
Courtney Foster <cfoster649@gmail.com>
2021-12-27 18:14:56 -0800

    Changed 'symbols' to 'special characters' throughout js for cohesiveness + js notes

--- script.js
+++ script.js
@@ -10,32 +10,31 @@ function writePassword() {
   var password = generatePassword()
   var passwordText = document.querySelector("#password");
 
+  //Turns pwd array into string of characters to make final password//
   passwordText.value = password.join('');
 
 }
 
-
-//////////////////////////////////////////////////////////////////
-
-
-
-
-
+//Function to create password//
 function generatePassword() {
 
+  //array containing password letters//
   const pwd = [];
 
+  //Prompts user to choose password b/w 8-128 characters, if outside of range prompt alerts and closes, user will have to click button again to start over//
   var chooseLength = prompt("How many characters would you like your password to be? (Must be between 8 and 128)");
   if (chooseLength < 7 || chooseLength > 128) {
     alert("Try again - password must be between 8 and 128 characters");
     return
   }
 
+  //Asking user to choose if they want to use lower case letters, upper case letters, special characters, and/or numbers//
   var getLowerCase = confirm("Would you like to use lower case letters?");
   var getUpperCase = confirm("Would you like to use upper case letters?");
-  var getSymbol = confirm("Would you like to use special characters?");
+  var getSpecialChar = confirm("Would you like to use special characters?");
   var getNumber = confirm("Would you like to use numbers?");
 
+  //For loop to generate password letters//
   for (i = 0; i < chooseLength; i++) {
 
     if (getLowerCase) {
@@ -54,8 +53,8 @@ function generatePassword() {
       return pwd
     }
 
-    if (getSymbol) {
-      pwd.push(chooseSymbol())
+    if (getSpecialChar) {
+      pwd.push(chooseSpecialChar())
     }
 
     if (pwd.length == chooseLength) {
@@ -78,10 +77,8 @@ function generatePassword() {
 };
 
 
-
-
-
-var symbols = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "=", "|", ";", ":", "(", ")", "{", "}", "[", "]", ",", ".", "<", ">", "/", "?"]
+//Character options - lower case letters, upper case letters, and numbers refer to UTF-8 charset table values, special characters chosen from given array//
+var specialChar = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "=", "|", ";", ":", "(", ")", "{", "}", "[", "]", ",", ".", "<", ">", "/", "?"]
 
 function chooseLowerCase() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
@@ -92,6 +89,6 @@ function chooseUpperCase() {
 function chooseNumber() {
   return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
 };
-function chooseSymbol() {
-  return symbols[Math.floor(Math.random() * symbols.length)]
+function chooseSpecialChar() {
+  return specialChar[Math.floor(Math.random() * specialChar.length)]
 }
